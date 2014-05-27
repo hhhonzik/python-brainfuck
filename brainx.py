ef4bbf6ec3cbaab63e6e9c6f3131be9525305d5a
Jan Stepanovsky <me@janstepanovsky.cz>
2014-05-27 19:54:27 +0200

    Version: 0.4

--- brainx.py
+++ brainx.py
@@ -14,7 +14,7 @@ def main(argv=None):
     #sys args definition
     parser = argparse.ArgumentParser();
     parser.add_argument('file');
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1');
+    parser.add_argument('--version', action='version', version='%(prog)s 0.4');
     parser.add_argument('-l', '--brainloller', dest="type", action="store_const", const="brainloller", help="Jde o program v jazyce brainloller.");
     parser.add_argument('-c', '--braincopter', dest="type", action="store_const", const="braincopter", help="Jde o program v jazyce braincopter.");
 
