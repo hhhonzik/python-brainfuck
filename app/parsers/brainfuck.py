4d81f31ddae3e840adfb17abb35ee190b1607b09
Jan Stepanovsky <me@janstepanovsky.cz>
2014-05-27 12:26:06 +0200

    Brainfuck: implementation

--- app/parsers/brainfuck.py
+++ app/parsers/brainfuck.py
@@ -1,5 +1,7 @@
-from interface import parserInterface;
 # -*- coding: utf-8 -*-
+import math, sys, types;
+from interface import parserInterface;
+
 
 
 class BrainFuck (parserInterface):
@@ -8,17 +10,125 @@ class BrainFuck (parserInterface):
     def __init__(self, data, memory=b'\x00', memory_pointer=0):
         """Inicializace interpretru brainfucku."""
 
+        # print self.data;
+        if data is types.FileType:
+            self.data = data.read();
+        else:
+            self.data = data;
+
+
+        # file load
         # data programu
-        self.data = data
+        try:
+            # je to soubor
+            with data as _file:
+                self.data = _file.read()
+        except:
+            # výjimka -> rovnou data
+            self.data = data
 
         # inicializace proměnných
-        self.memory = memory
+        self.memory = bytearray(memory);
         self.memory_pointer = memory_pointer
-
-        # DEBUG a testy
 
         # a) paměť výstupu
         self.output = ''
+
+
+        self.decode(self.data);
+
+
+    #
+    # Operator functions
+    #
+    def getOperator(self, item):
+        return {
+            ">": self.operatorRight,
+            "<": self.operatorLeft,
+            "+": self.operatorPlus,
+            "-": self.operatorMinus,
+            ".": self.operatorEcho,
+            ",": self.operatorLoad,
+            "[": self.operatorFor
+        }.get(item);
+
+
+    # <for> - start cyklu
+    def operatorFor(self, code):
+        lefts = 1 #  sum( [ )
+        rights = 0 # sum( ] )
+
+        i = 1    # prvni je [
+
+        while i < len(code):
+            if code[i] == '[':
+                lefts += 1
+            elif code[i] == ']':
+                rights += 1
+            if lefts == rights:
+                break
+            i += 1
+
+        while self.memory[self.memory_pointer] != 0:
+            self.decode(code[1:i]);
+
+        return i + 1; #posunout o 1
+
+
+    # nacteni hodnoty
+    def operatorLoad(self):
+        self.memory[self.memory_pointer] = ord(sys.stdin.read(1))
+        return;
+
+
+    # vypis bunky
+    def operatorEcho(self):
+        val = chr(self.memory[self.memory_pointer]);
+        self.output += val;
+        # print val;
+
+
+    # snizenihodnoty bunky
+    def operatorMinus(self):
+        if self.memory[self.memory_pointer ] == 0:
+            self.memory[self.memory_pointer ] = 255;
+        else:
+            self.memory[self.memory_pointer ] -= 1;
+
+
+
+    # zvyseni hodnoty bunky
+    def operatorPlus(self):
+        self.memory[self.memory_pointer ] = (self.memory[self.memory_pointer ] + 1) % 256;
+
+    # posun doprava
+    def operatorRight(self):
+        self.memory_pointer += 1
+        # zvyseni pole
+        if self.memory_pointer == len(self.memory):
+            self.memory += bytearray([0])
+
+    def operatorLeft(self):
+        # minimalni hodnota 0
+        self.memory_pointer = max(0, self.memory_pointer - 1);
+
+    #
+    # BrainFuck decoder
+    #
+    def decode(self, code):
+        i = 0;
+        while i < len(code):
+            operator = self.getOperator(code[i] );
+            if operator == None:
+                print "Neexistujici operator: {0}:{1}".format(i,code[i]);
+                i += 1;
+            else:
+                if code[i] == "[":
+                    i += operator(code[i:]); #  add string
+                else:
+                    operator();
+                    i += 1;
+
 
     #
     # pro potřeby testů
